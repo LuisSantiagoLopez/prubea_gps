bf479952e905ab88abf288e0c1cce5283ec847f3
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-06-06 08:09:45 -0600

    fucking tutorial didn't name the view correctly

--- shops/views.py
+++ shops/views.py
@@ -8,7 +8,7 @@ latitude = 25.761681
 
 user_location = Point(longitude, latitude, srid=4326)
 
-class Home(generic.ListView):
+class ShopList(generic.ListView):
     model = Shop 
     context_object_name = 'shops' 
     queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6]
