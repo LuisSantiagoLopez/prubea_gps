1a0ac6066390dfce5bf7c86eb4c804df02e5b54d
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-06-06 08:15:59 -0600

    fucking tutorial didn't name the view correctly

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,7 +62,7 @@ ROOT_URLCONF = "nearbyshops.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -133,5 +134,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
